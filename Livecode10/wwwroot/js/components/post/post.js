bf4d3a8328e5cc2f0a090d99b686ee1ef49e9356
Filip Germanek <filipgermanek@yahoo.com>
2018-12-13 12:23:32 +0100

    layout changes, get posts and single post

--- Livecode10/wwwroot/js/components/post/post.js
+++ Livecode10/wwwroot/js/components/post/post.js
@@ -1,6 +1,14 @@
-﻿define(['knockout', 'postman'], function (ko, postman) {
+﻿define(['knockout', 'dataService', 'postman'], function (ko, ds, postman) {
     return function (params) {
-        var title = "dd"; //params.post.title;
+        var post = ko.observable();
+        var postId = 13649012;
+        var title = "testttt", body;
+        ds.getPost(postId, function(data) {
+            console.log("data post", data);
+            post(data);
+        });
+       // title = post.title;
+       // body = post.body;
         return {
             title
         };
